c0481c0c99ddc20677d4fac929919c8be2b79ade
markk <markkonijnenburg@teamrembrandts.com>
2023-12-05 19:18:02 +0100

    Test voor commit in Git

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystem/ExampleSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystem/ExampleSubsystem.java
@@ -16,6 +16,7 @@ public class ExampleSubsystem {
 
     //Declare motor and servo objects
     private  DcMotor myMotor;
+    private double testForCommitGit;
     private Servo myServo;
 
     //Define positions and other variables of the subsystem
